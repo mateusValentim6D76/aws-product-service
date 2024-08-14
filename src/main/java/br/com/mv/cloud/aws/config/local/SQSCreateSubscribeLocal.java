d54e865b21f40da382877ec301f904f4c4da1beb
mateusValentim6D76 <mateus.valentim19@hotmail.com>
2024-08-13 23:40:26 -0300

    Alterado tipo de injeção de AmazonSNSClient para AmazonSNS em SQSCreateSubscribeLocal

--- src/main/java/br/com/mv/cloud/aws/config/local/SQSCreateSubscribeLocal.java
+++ src/main/java/br/com/mv/cloud/aws/config/local/SQSCreateSubscribeLocal.java
@@ -5,33 +5,30 @@ import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.sns.AmazonSNS;
 import com.amazonaws.services.sns.AmazonSNSClient;
-import com.amazonaws.services.sns.model.CreateTopicRequest;
 import com.amazonaws.services.sns.model.Topic;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Bean;
+import com.amazonaws.services.sns.util.Topics;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClient;
+import com.amazonaws.services.sqs.model.CreateQueueRequest;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
 @Profile("local")
-@Slf4j
-public class SnsCreateConfigLocal {
+public class SQSCreateSubscribeLocal {
 
-    @Bean
-    public AmazonSNS snsClient() {
-        return AmazonSNSClient.builder()
+
+    public SQSCreateSubscribeLocal(AmazonSNS amazonSNSClient, @Qualifier("productEventsTopic") Topic productEventsTopic) {
+        AmazonSQS sqsClient = AmazonSQSClient.builder()
                 .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://localhost:4566", Regions.US_EAST_1.getName()))
                 .withCredentials(new DefaultAWSCredentialsProviderChain())
                 .build();
-    }
 
-    @Bean(name = "productEventsTopic")
-    public Topic snsProductEventsTopic(AmazonSNS snsClient) {
-        CreateTopicRequest createTopicRequest = new CreateTopicRequest("product-events");
-        String topicArn = snsClient.createTopic(createTopicRequest).getTopicArn();
+        String productEventsQueueUrl = sqsClient.createQueue(new CreateQueueRequest("product-events-queue")).getQueueUrl();
 
-        log.info("SNS topic ARN: {}", topicArn);
-        return new Topic().withTopicArn(topicArn);
+        Topics.subscribeQueue(amazonSNSClient, sqsClient, productEventsTopic.getTopicArn(), productEventsQueueUrl);
+
+
     }
 }
